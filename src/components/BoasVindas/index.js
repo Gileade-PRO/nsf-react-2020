b8e85cb472902abfe879678d5dcb8b024d3c919d
Gileade-PRO <ranedgileadesilva@gmail.com>
2020-09-21 23:31:58 -0300

    first commit

---
+++ src/components/BoasVindas/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './BoasVindas.css'
+
+export default function BoasVindas() {
+    return(
+        <div>
+            <h1 className="editar"> Ola, seja bem vindo </h1>
+            <h2> Aperte o botão para começar </h2>
+        </div>
+    );
+}
+
